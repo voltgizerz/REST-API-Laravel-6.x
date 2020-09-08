262da2288ca15d50b1109f01ebdf655a2e8cd332
Felix Fernando <54938467+voltgizerz@users.noreply.github.com>
2020-09-08 20:06:09 +0700

    06 Endpoint Membuat Quote

---
+++ app/Http/Controllers/Api/QuoteController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Quote;
+use App\Http\Resources\QuoteResource;
+use Illuminate\Http\Request;
+
+class QuoteController extends Controller
+{
+    public function store(Request $request){
+
+        $this->validate($request,[
+            'message'=>'required',
+        ]);
+
+        $quote=Quote::create([
+            'user_id'=>auth()->id(),
+            'message'=>$request->message,
+        ]);
+
+        return new QuoteResource($quote);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@
 Route::post('register','Api\RegisterController@action');
 Route::post('login','Api\LoginController@action');
 Route::get('me','Api\UserController@me')->middleware('auth:api');
+Route::post('quote','Api\QuoteController@store')->middleware('auth:api');
